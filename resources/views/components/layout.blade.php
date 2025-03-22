567c50beda0b8980aab047e3d743a29b62543a99
r <r@craig.ooo>
2025-03-22 17:22:32 +0000

    30 Days to Learn Laravel Day 5

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,36 +7,7 @@
 
     <title>Contact</title>
 </head>
-<body>
-
-{{--<nav>--}}
-{{--    <x-nav-link href="/">Home</x-nav-link>--}}
-{{--    <x-nav-link href="/about">About</x-nav-link>--}}
-{{--    <x-nav-link href="/contact">Contact</x-nav-link>--}}
-
-{{--</nav>--}}
-
-
-{{--Short Form--}}
-{{--{{ $slot {--Long form--}}
-{{--<?php echo $slot ?>--}}{{--}}--}}
-
-
-{{--</body>--}}
-{{--</html>--}}
-
-
-
-
-
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
+<body class="h-full">
 <div class="min-h-full">
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -48,14 +19,9 @@
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-{{--                            <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</a>--}}
-{{--                            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>--}}
-{{--                            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>--}}
-{{--                            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>--}}
-{{--                            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a>--}}
-                                <x-nav-link class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page" href="/">Home</x-nav-link>
-                                <x-nav-link class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" href="/about">About</x-nav-link>
-                                <x-nav-link class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" href="/contact">Contact</x-nav-link>
+                            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                            <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                         </div>
                     </div>
                 </div>
