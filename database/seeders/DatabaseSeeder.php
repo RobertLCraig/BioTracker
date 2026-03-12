5d8c18cd6209e61c87c1ab46e9194b9362764636
Robert Craig <robert@example.com>
2026-03-12 10:59:30 +0000

    Do not overwrite an existing username and password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,10 +20,10 @@ class DatabaseSeeder extends Seeder
             AchievementSeeder::class,
         ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            ['name' => 'Test User', 'password' => bcrypt('password')],
+        );
 
         $this->call([
             DemoDataSeeder::class,
